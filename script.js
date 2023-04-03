ca8dec64df6449bc79519b392798ef57915adff6
unknown <mko2001@op.pl>
2023-04-03 13:34:27 +0200

    working contact section

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ toggleBtn.onclick = function () {
   const isOpen = dropDownMenu.classList.contains("open");
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
+
+function sendEmail() {
+  Email.send({
+    SecureToken: "52c4c2f0-b304-4d5a-8827-aad156837dde",
+    To: "meyol2001@gmail.com",
+    From: "mko2001@op.pl",
+    Subject: "This is the subject",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Wiadomość: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Wiadomość została wysłana pomyślnie"));
+}
